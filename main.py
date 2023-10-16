b2842c20d345f4ebc40c8ebca0ca7d56c309412a
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-10-17 09:29:10 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
+import sys
 from time import sleep
-
-# CHANGE DIRECTORIES AND USER NAME.
-downloads = "C:/Users/CHANGE/Downloads/"
-move_to = "C:/Users/CHANGE/Desktop/CHANGE/"
-
-# Feel free to add any more file types, values are the names of the folder in which the file types are stored.
+# CHANGE DIRECTORIES
+downloads = "Default/downloads/folder"
+move_to = "Where/files/will/be/stored"
 file_types = {
     ".mp4": "Videos", 
     ".mov": "Videos", 
@@ -18,33 +16,48 @@ file_types = {
     ".jpeg": "Images",
     ".webp": "Images",
     ".jpg": "Images",
+    ".ico": "Images",
     ".pdf": "Web",
     ".html": "Web",
     ".mp3": "Audio files",
     ".wav": "Audio files",
     ".ogg": "Audio files",
     ".exe": "Executables",
-    ".zip": "Zip files"
+    ".zip": "Zip files",
+    ".txt": "Text files",
+    ".py": "Scripts",
+    ".css": "Scripts",
+    ".js": "Scripts",
+    ".java": "Scripts",
+    ".c": "Scripts",
+    ".cpp": "Scripts"
 }
-# Makes program work, coded this 5 minutes ago and have no idea what's going on.
-def recurse():
-    # So it doesn't recurst infinitely, remove/comment to sort a large amount.
-    sleep(7)
+def recurse(num):
+    # Remove sleep function for cleaning up all files
+    sleep(5)
+    # Checks if file is still being downloaded
+    if os.listdir(downloads)[0].endswith(".crdownload"):
+        return recurse(num+1)
     try:
-        for i in file_types:
-            # Checks if file ends with extension
-            if os.listdir(downloads)[0].endswith(i):
-                # Uncomment below for debugging
-                #print(os.listdir(downloads)[0]+ " ends with "+i)
-                # Moves file from downloads/filename.ext, to directory/filetype/filename.ext
-                os.replace(downloads+os.listdir(downloads)[0], move_to+file_types[i]+"/"+os.listdir(downloads)[0])
-                return recurse()
-        # If file has no matching extensions, move to other.
-        os.replace(downloads+os.listdir(downloads)[0], move_to+"other/"+os.listdir(downloads)[0])
-        return recurse()
-    # Temporary fix, will add if statement to check for files in dir
+        # Declare downloaded file
+        downloaded_file = os.listdir(downloads)[0+num]
     except IndexError:
-        return recurse()    
+        # If downloads folder is empty
+        return recurse(0)
+    for i in file_types:
+        if downloaded_file.endswith(i):
+            if downloaded_file not in os.listdir(move_to+file_types[i]):
+                os.replace(downloads+downloaded_file, move_to+file_types[i]+"/"+downloaded_file)
+                print("Successfull")
+                print(f"Moved {downloaded_file} to {move_to+file_types[i]}/")
+                return recurse(0)
+            else:
+                # If there's already a file with the same name
+                print(f"'{downloaded_file}' already exists in '{move_to+file_types[i]}'")
+                return recurse(num+1)
+    print("Does not match with anything, moving to other")
+    os.replace(downloads+downloaded_file, move_to+"other/"+downloaded_file)
+    return recurse(0)
         
 
-recurse()
+recurse(0)
