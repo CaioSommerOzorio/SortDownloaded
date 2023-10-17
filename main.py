9d3b0b7729e038c3ed1a0d6768ade327329e3306
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-10-17 17:27:26 +1100

    Added error handling to main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import sys
 from time import sleep
+
 # CHANGE DIRECTORIES
-downloads = "Default/downloads/folder"
-move_to = "Where/files/will/be/stored"
+
+downloads = "C:/Users/farof/Downloads/"
+move_to = "C:/Users/farof/Onedrive/Desktop/Downloaded/"
+
 file_types = {
     ".mp4": "Videos", 
     ".mov": "Videos", 
@@ -19,6 +22,7 @@ file_types = {
     ".ico": "Images",
     ".pdf": "Web",
     ".html": "Web",
+    ".html": "Web",
     ".mp3": "Audio files",
     ".wav": "Audio files",
     ".ogg": "Audio files",
@@ -32,32 +36,74 @@ file_types = {
     ".c": "Scripts",
     ".cpp": "Scripts"
 }
-def recurse(num):
-    # Remove sleep function for cleaning up all files
-    sleep(5)
+
+def main(num: int):
     # Checks if file is still being downloaded
     if os.listdir(downloads)[0].endswith(".crdownload"):
-        return recurse(num+1)
+        print("File has not finished downloading")
+        sleep(5)
+        return main(num+1)
     try:
         # Declare downloaded file
         downloaded_file = os.listdir(downloads)[0+num]
     except IndexError:
         # If downloads folder is empty
-        return recurse(0)
+        sleep(5)
+        return main(0)
     for i in file_types:
         if downloaded_file.endswith(i):
-            if downloaded_file not in os.listdir(move_to+file_types[i]):
-                os.replace(downloads+downloaded_file, move_to+file_types[i]+"/"+downloaded_file)
-                print("Successfull")
-                print(f"Moved {downloaded_file} to {move_to+file_types[i]}/")
-                return recurse(0)
-            else:
-                # If there's already a file with the same name
-                print(f"'{downloaded_file}' already exists in '{move_to+file_types[i]}'")
-                return recurse(num+1)
-    print("Does not match with anything, moving to other")
-    os.replace(downloads+downloaded_file, move_to+"other/"+downloaded_file)
-    return recurse(0)
-        
-
-recurse(0)
+            try:
+                if downloaded_file not in os.listdir(move_to+file_types[i]):
+                    try:
+                        os.replace(downloads+downloaded_file, move_to+file_types[i]+"/"+downloaded_file)
+                    except PermissionError:
+                        print("File opened somewhere else.")
+                        sleep(5)
+                        return main(+1)
+                    print(f"Moved {downloaded_file} to {move_to+file_types[i]}/")
+                    sleep(5)
+                    return main()
+                else:
+                    # If there's already a file with the same name
+                    print(f"'{downloaded_file}' already exists in '{move_to+file_types[i]}'")
+                    sleep(5)
+                    return main(num+1)
+            except FileNotFoundError:
+                print(f"{file_types[i]} folder does not exist, making directory.")
+                os.mkdir(move_to+file_types[i])
+                print("Directory made")
+                if downloaded_file not in os.listdir(move_to+file_types[i]):
+                    try:
+                        os.replace(downloads+downloaded_file, move_to+file_types[i]+"/"+downloaded_file)
+                    except PermissionError:
+                        print("File opened somewhere else.")
+                        sleep(5)
+                        return main(num+1)
+                    print(f"Moved [{downloaded_file}] to {move_to+file_types[i]}/")
+                    sleep(5)
+                    return main(0)
+                else:
+                    # If there's already a file with the same name
+                    print(f"[{downloaded_file}] already exists in '{move_to+file_types[i]}'")
+                    return main(num+1)
+    try:
+        os.replace(downloads+downloaded_file, move_to+"other/"+downloaded_file)
+        sleep(5)
+        return main(0)
+    except FileNotFoundError:
+        # Folder not found
+        print(f"{file_types} folder does not exist, making directory.")
+        os.makedirs(move_to+"other")
+        os.replace(downloads+downloaded_file, move_to+"other/"+downloaded_file)
+        print(f"Moved [{downloaded_file}] to {move_to}/other")
+        sleep(5)
+        return main(0)
+    except PermissionError:
+        print(f"[{downloaded_file}] is opened somewhere else.")
+        sleep(5)
+        return main(num+1)
+    
+try:
+    main(0)
+except KeyboardInterrupt:
+    print("\nExiting...")
