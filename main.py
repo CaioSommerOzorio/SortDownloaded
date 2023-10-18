68d4b922ab0b50c031fa3b3d56ae193bf45fd237
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-10-19 10:25:46 +1100

    Fixed typos and added directory checking

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ init()
 downloads = "C:/Default/download/directory/"
 move_to = "C:/Move/to/folder/"
 
+folders = ["Videos", "Images", "Web", "Audio files", "Executables", "Zip files", "Text files", "Scripts"]
+
 file_types = {
     ".mp4": "Videos", 
     ".mov": "Videos", 
@@ -38,6 +40,15 @@ file_types = {
     ".cpp": "Scripts"
 }
 
+def make_dirs():
+    print(f"{Fore.GREEN}Checking for directories...")
+    for i in folders:
+        if i not in os.listdir(move_to):
+            print(f"\n{Fore.BLUE}[{i}]{Fore.RED} does not exist, making directory.")
+            os.makedirs(f"{move_to}/{i}")
+            print(f"{Fore.BLUE}[{i}]{Fore.GREEN} directory has been made.\n")
+    print("Directories are up to date.\n")
+
 def main(num: int):
     try:
         # Declare downloaded file
@@ -53,29 +64,29 @@ def main(num: int):
                     try:
                         os.replace(downloads+downloaded_file, move_to+file_types[i]+"/"+downloaded_file)
                     except PermissionError:
-                        print(Fore.BLUE + downloaded_file + Fore.RED + " is opened somewhere else.")
+                        print(f"{Fore.BLUE}[{downloaded_file}]{Fore.RED} is opened somewhere else, cannot move.\n")
                         sleep(5)
                         return main(num+1)
-                    print(f"{Fore.GREEN}Moved "+ Fore.BLUE + downloaded_file + f"{Fore.GREEN}to {Fore.GREEN}[{move_to+file_types[i]}/]")
+                    print(f"{Fore.GREEN}Moved {Fore.BLUE}[{downloaded_file}]{Fore.GREEN} to {Fore.BLUE}[{move_to+file_types[i]}/]\n")
                     sleep(5)
                     return main(0)
                 else:
                     # If there's already a file with the same name
-                    print(f"{Fore.BLUE}[{downloaded_file}] {Fore.RED}already exists in {Fore.BLUE}[{move_to+file_types[i]}]")
-                    print(f"{Fore.GREEN}Making new name...")
+                    print(f"{Fore.BLUE}[{downloaded_file}] {Fore.RED}already exists in {Fore.BLUE}[{move_to+file_types[i]}]\n")
+                    print(f"{Fore.GREEN}Making new name...\n")
                     new_name = uuid.uuid4().hex+"."+downloaded_file
-                    print(f"{Fore.BLUE}[{downloaded_file}]{Fore.GREEN} has been renamed to {Fore.BLUE}[{new_name}]")
+                    print(f"{Fore.BLUE}[{downloaded_file}]{Fore.GREEN} has been renamed to {Fore.BLUE}[{new_name}]\n")
                     os.replace(downloads+downloaded_file, downloads+new_name)
                     return main(0)
             except FileNotFoundError:
-                print(f"{Fore.BLUE}{file_types[i]}{Fore.RED} folder does not exist, making directory.")
+                print(f"{Fore.BLUE}{file_types[i]}{Fore.RED} folder does not exist, making directory.\n")
                 os.mkdir(move_to+file_types[i])
-                print(f"{Fore.GREEN}Directory made")
+                print(f"{Fore.GREEN}Directory made\n")
                 if downloaded_file not in os.listdir(move_to+file_types[i]):
                     try:
                         os.replace(downloads+downloaded_file, move_to+file_types[i]+"/"+downloaded_file)
                     except PermissionError:
-                        print(f"{Fore.BLUE}[{downloaded_file}] {Fore.GREEN}opened somewhere else.")
+                        print(f"{Fore.BLUE}[{downloaded_file}] {Fore.GREEN} is Opened somewhere else.")
                         sleep(5)
                         return main(num+1)
                     print(f"{Fore.GREEN}Moved {Fore.BLUE}[{downloaded_file}]{Fore.GREEN} to {Fore.BLUE}{move_to+file_types[i]}/")
@@ -83,9 +94,8 @@ def main(num: int):
                     return main(0)
                 else:
                     # If there's already a file with the same name
-                    print(f"{Fore.Blue}[{downloaded_file}] {Fore.RED}already exists in {Fore.BLUE}[{move_to+file_types[i]}]")
-                    
-                    print("Renamed to ")
+                    print(f"{Fore.Blue}[{downloaded_file}] {Fore.RED}already exists in {Fore.BLUE}[{move_to+file_types[i]}]\n")
+                    print(f"Renamed to {new_name}\n")
                     return main(num+1)
     try:
         os.replace(downloads+downloaded_file, move_to+"other/"+downloaded_file)
@@ -93,17 +103,19 @@ def main(num: int):
         return main(0)
     except FileNotFoundError:
         # Folder not found
-        print(f"{Fore.BLUE}{file_types}{Fore.RED} folder does not exist, making directory.")
+        print(f"{Fore.BLUE}{file_types}{Fore.RED} folder does not exist, making directory.\n")
         os.makedirs(move_to+"other")
         os.replace(downloads+downloaded_file, move_to+"other/"+downloaded_file)
-        print(Fore.GREEN + "Moved " + Fore.BLUE + f"[{downloaded_file}]" + Fore.GREEN + "to {move_to}/other")
+        print(f"{Fore.GREEN}Moved {Fore.BLUE}[{downloaded_file}]{Fore.GREEN} to {Fore.BLUE}[{move_to}/other]\n")
         sleep(5)
         return main(0)
     except PermissionError:
-        print(Fore.BLUE + f"[{downloaded_file}]" + Fore.RED + " is opened somewhere else.")
+        print(f"{Fore.BLUE}[{downloaded_file}]{Fore.RED} is opened somewhere else, cannot move.\n")
         sleep(5)
         return main(num+1)
     
+
+make_dirs()
 
 try:
     main(0)
